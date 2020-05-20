d01c151fc538050aa158b5db8ec32b786a7f6c61
gustaveH <ghabiman@gmail.com>
2020-05-20 13:51:48 -0400

    Securing credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,10 @@
 class Stock < ApplicationRecord
+    def self.new_lookup(ticker_symbol)
+        client = IEX::Api::Client.new(
+            publishable_token: 'Tpk_1ce4e71851f84de28cef27f4fd3d9b7d',
+            secret_token: 'secret_token',
+            endpoint: 'https://sandbox.iexapis.com/v1'
+          )
+        client.price(ticker_symbol)
+    end
 end
